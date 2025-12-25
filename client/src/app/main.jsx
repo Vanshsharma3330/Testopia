f9de3f2c42dbf186cb2d316cd7aab488d9025000
VanshSharma <vanshsharma2786@gmail.com>
2025-12-25 13:15:17 +0530

    Update main.jsx

--- client/src/app/main.jsx
+++ client/src/app/main.jsx
@@ -3,10 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "../assets/styles/globals.css";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
-import ToastProvider from "../context/toastContext.jsx";
+import ToastProvider from "../context/ToastContext.jsx";
 import AuthProvider from "../context/AuthContext.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
