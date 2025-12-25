5a22ce84caa83867578a17e3b84f138577fef35a
VanshSharma <vanshsharma2786@gmail.com>
2025-12-25 14:10:04 +0530

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const allowedOrigins = [
-  process.env.CLIENT_URL,
+  process.env.ACCESS_URL,
   "http://localhost:5173"
 ];
 
